0cd6aba693c26f21ee5b213ce1f397195cfe0716
Prokhor Huletski <169000460+Fooggyyyy@users.noreply.github.com>
2024-11-21 14:56:19 +0300

    Update Lab7.js

--- JS/Лаба 7/Lab7.js
+++ JS/Лаба 7/Lab7.js
@@ -118,3 +118,112 @@ Object.defineProperty(array, 'sum', {
     }
 });
 console.log(`Сумма массива: ${array.sum}`);
+
+// Задание 9: Переопределение дескрипторов
+console.log("Задание 9");
+let car = {};
+Object.defineProperties(car, {
+    make: {
+        value: "Tesla",
+        writable: true,
+        configurable: true,
+        enumerable: true,
+    },
+    model: {
+        value: "Model S",
+        writable: true,
+        configurable: true,
+        enumerable: true,
+    },
+    year: {
+        value: 2023,
+        writable: true,
+        configurable: true,
+        enumerable: true,
+    },
+});
+
+// Переопределяем дескрипторы, чтобы свойства стали неизменяемыми
+Object.defineProperties(car, {
+    make: { writable: false, configurable: false },
+    model: { writable: false, configurable: false },
+    year: { writable: false, configurable: false },
+});
+
+console.log(car);
+try {
+    car.make = "Ford";
+} catch (e) {
+    console.log("Нельзя изменить свойство make!");
+}
+
+// Задание 10: получение площади в прямоугольнике
+console.log("Задание 10");
+let rectangle = {
+    _width: 0,
+    _height: 0,
+
+    get width() {
+        return this._width;
+    },
+    set width(value) {
+        if (value > 0) this._width = value;
+        else console.log("Ширина должна быть положительным числом!");
+    },
+
+    get height() {
+        return this._height;
+    },
+    set height(value) {
+        if (value > 0) this._height = value;
+        else console.log("Высота должна быть положительным числом!");
+    },
+
+    get area() {
+        return this._width * this._height;
+    },
+};
+
+rectangle.width = 5;
+rectangle.height = 10;
+console.log(`Ширина: ${rectangle.width}, Высота: ${rectangle.height}, Площадь: ${rectangle.area}`);
+
+// Задание 11: Получение полного имени
+console.log("Задание 11");
+let user = {
+    _firstName: "Иван",
+    _lastName: "Петров",
+
+    get firstName() {
+        return this._firstName;
+    },
+    set firstName(value) {
+        this._firstName = value;
+    },
+
+    get lastName() {
+        return this._lastName;
+    },
+    set lastName(value) {
+        this._lastName = value;
+    },
+
+    get fullName() {
+        return `${this._firstName} ${this._lastName}`;
+    },
+    set fullName(value) {
+        let parts = value.split(" ");
+        if (parts.length === 2) {
+            this._firstName = parts[0];
+            this._lastName = parts[1];
+        } else {
+            console.log("Введите полное имя в формате 'Имя Фамилия'.");
+        }
+    },
+};
+
+console.log(user.fullName); // Иван Петров
+user.fullName = "Алексей Иванов";
+console.log(user.fullName); // Алексей Иванов
+console.log(`Имя: ${user.firstName}, Фамилия: ${user.lastName}`);
+
